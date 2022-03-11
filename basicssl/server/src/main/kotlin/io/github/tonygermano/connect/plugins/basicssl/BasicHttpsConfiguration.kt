5a73a8e77abb0b3f524aea55449f8743999485d0
Alessandro Piroddi <alessandro.piroddi@inpeco.com>
2022-03-11 16:28:23 +0100

    Reformat code, add copyright, update references
      Update code so that it works with Mirth Connect 3.12.0

--- basicssl/server/src/main/kotlin/io/github/tonygermano/connect/plugins/basicssl/BasicHttpsConfiguration.kt
+++ basicssl/server/src/main/kotlin/io/github/tonygermano/connect/plugins/basicssl/BasicHttpsConfiguration.kt
@@ -2,7 +2,8 @@
  * Original work Copyright (c) Mirth Corporation. All rights reserved. http://www.mirthcorp.com
  * 
  * Modified work Copyright 2019 Tony Germano
- * 
+ * Modified work Copyright 2022 Alessandro Piroddi
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
@@ -11,11 +12,11 @@
  * defined by the Mozilla Public License, v. 2.0.
  * 
  * This file is derived from the following files:
- * https://github.com/nextgenhealthcare/connect/blob/3.8.1/server/src/com/mirth/connect/connectors/http/DefaultHttpConfiguration.java
- * https://github.com/nextgenhealthcare/connect/blob/3.8.1/server/src/com/mirth/connect/server/MirthWebServer.java
+ * https://github.com/nextgenhealthcare/connect/blob/3.12.0/server/src/com/mirth/connect/connectors/http/DefaultHttpConfiguration.java
+ * https://github.com/nextgenhealthcare/connect/blob/3.12.0/server/src/com/mirth/connect/server/MirthWebServer.java
  */
 
-package io.github.tonygermano.connect.plugins.basicssl;
+package io.github.tonygermano.connect.plugins.basicssl
 
 import com.mirth.connect.client.core.PropertiesConfigurationUtil
 import com.mirth.connect.connectors.http.HttpConfiguration
@@ -43,13 +44,14 @@ import java.io.InputStream
 import java.security.KeyStore
 import javax.servlet.ServletRequest
 
+@SuppressWarnings("unused")
 class BasicHttpsConfiguration : HttpConfiguration {
 
-    private val configurationController = ControllerFactory.getFactory().createConfigurationController();
+    private val configurationController = ControllerFactory.getFactory().createConfigurationController()
 
     override fun configureConnectorDeploy(connector: Connector) {
         if (connector is HttpDispatcher) {
-            configureSocketFactoryRegistry(null, connector.getSocketFactoryRegistry());
+            configureSocketFactoryRegistry(null, connector.socketFactoryRegistry)
         }
     }
 
@@ -58,60 +60,71 @@ class BasicHttpsConfiguration : HttpConfiguration {
     override fun configureReceiver(connector: HttpReceiver) {
 
 
-        val mirthProperties = getMirthProperties();
+        val mirthProperties = getMirthProperties()
 
-        val keyStore = KeyStore.getInstance("JCEKS");
-        File(mirthProperties.getString("keystore.path")).inputStream().use() {
-            keyStore.load(it, mirthProperties.getString("keystore.storepass").toCharArray());
+        val keyStore = KeyStore.getInstance("JCEKS")
+        File(mirthProperties.getString("keystore.path")).inputStream().use {
+            keyStore.load(it, mirthProperties.getString("keystore.storepass").toCharArray())
         }
 
-        val contextFactory = SslContextFactory();
-        contextFactory.setKeyStore(keyStore);
-        contextFactory.setCertAlias("mirthconnect");
-        contextFactory.setKeyManagerPassword(mirthProperties.getString("keystore.keypass"));
+        val contextFactory = SslContextFactory()
+        contextFactory.keyStore = keyStore
+        contextFactory.certAlias = "mirthconnect"
+        contextFactory.setKeyManagerPassword(mirthProperties.getString("keystore.keypass"))
+        contextFactory.endpointIdentificationAlgorithm = null
 
-        val config = org.eclipse.jetty.server.HttpConfiguration();
-        config.setSecureScheme("https");
-        config.setSecurePort(mirthProperties.getInt("https.port"));
-        config.addCustomizer(SecureRequestCustomizer());
-        config.setSendServerVersion(false);
-        config.setSendXPoweredBy(false);
+
+        val config = org.eclipse.jetty.server.HttpConfiguration()
+        config.secureScheme = "https"
+        config.securePort = mirthProperties.getInt("https.port")
+        config.addCustomizer(SecureRequestCustomizer())
+        config.sendServerVersion = false
+        config.sendXPoweredBy = false
 
         val sslConnector = ServerConnector(
-            connector.getServer(),
+            connector.server,
             SslConnectionFactory(contextFactory, HttpVersion.HTTP_1_1.asString()),
             HttpConnectionFactory(config)
-        );
+        )
 
-        sslConnector.setName("ssllistener");
-        sslConnector.setHost(connector.getHost());
-        sslConnector.setPort(connector.getPort());
-        sslConnector.setIdleTimeout(connector.getTimeout().toLong());
-        connector.getServer().addConnector(sslConnector);
+        sslConnector.name = "ssllistener"
+        sslConnector.host = connector.host
+        sslConnector.port = connector.port
+        sslConnector.idleTimeout = connector.timeout.toLong()
+        connector.server.addConnector(sslConnector)
 
-        val lowResourceMonitor = LowResourceMonitor(connector.getServer());
-        lowResourceMonitor.setMonitoredConnectors(listOf(sslConnector));
+        val lowResourceMonitor = LowResourceMonitor(connector.server)
+        lowResourceMonitor.monitoredConnectors = listOf(sslConnector)
         // If the number of connections open reaches 200
-        lowResourceMonitor.setMaxConnections(200);
+        lowResourceMonitor.maxConnections = 200
         // Then close connections after 200 seconds, which is the default MaxIdleTime value. This should affect existing connections as well.
-        lowResourceMonitor.setLowResourcesIdleTimeout(200000);
+        lowResourceMonitor.lowResourcesIdleTimeout = 200000
 
-        contextFactory.setExcludeProtocols();
-        contextFactory.setExcludeCipherSuites();
-        contextFactory.setIncludeProtocols(*MirthSSLUtil.getEnabledHttpsProtocols(configurationController.getHttpsServerProtocols()));
-        contextFactory.setIncludeCipherSuites(*MirthSSLUtil.getEnabledHttpsCipherSuites(configurationController.getHttpsCipherSuites()));
+        contextFactory.setExcludeProtocols()
+        contextFactory.setExcludeCipherSuites()
+        contextFactory.setIncludeProtocols(*MirthSSLUtil.getEnabledHttpsProtocols(configurationController.httpsServerProtocols))
+        contextFactory.setIncludeCipherSuites(*MirthSSLUtil.getEnabledHttpsCipherSuites(configurationController.httpsCipherSuites))
     }
 
-    override fun configureDispatcher(connector: HttpDispatcher, connectorProperties: HttpDispatcherProperties)  {}
+    override fun configureDispatcher(connector: HttpDispatcher, connectorProperties: HttpDispatcherProperties) {}
 
-    override fun configureSocketFactoryRegistry(properties: ConnectorPluginProperties?, registry: RegistryBuilder<ConnectionSocketFactory>) {
-        val enabledProtocols = MirthSSLUtil.getEnabledHttpsProtocols(configurationController.getHttpsClientProtocols());
-        val enabledCipherSuites = MirthSSLUtil.getEnabledHttpsCipherSuites(configurationController.getHttpsCipherSuites());
-        val sslConnectionSocketFactory = SSLConnectionSocketFactory(SSLContexts.createSystemDefault(), enabledProtocols, enabledCipherSuites, NoopHostnameVerifier.INSTANCE);
-        registry.register("https", sslConnectionSocketFactory);
+    override fun configureSocketFactoryRegistry(
+        properties: ConnectorPluginProperties?,
+        registry: RegistryBuilder<ConnectionSocketFactory>
+    ) {
+        val enabledProtocols = MirthSSLUtil.getEnabledHttpsProtocols(configurationController.httpsClientProtocols)
+        val enabledCipherSuites =
+            MirthSSLUtil.getEnabledHttpsCipherSuites(configurationController.httpsCipherSuites)
+        val sslConnectionSocketFactory = SSLConnectionSocketFactory(
+            SSLContexts.createSystemDefault(),
+            enabledProtocols,
+            enabledCipherSuites,
+            NoopHostnameVerifier.INSTANCE
+        )
+        registry.register("https", sslConnectionSocketFactory)
     }
 
-    override fun getRequestInformation(request: ServletRequest): Map<String, Any> = emptyMap<String, Any>();
+    override fun getRequestInformation(request: ServletRequest): Map<String, Any> = emptyMap<String, Any>()
 
     @Throws(FileNotFoundException::class, ConfigurationException::class)
     private fun getMirthProperties(): PropertiesConfiguration {
